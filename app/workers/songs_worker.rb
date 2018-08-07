884bd58469249977146981c29133c55cbe84219b
Katleiah Ramos <katleiahramos@gmail.com>
2018-08-07 18:49:34 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -5,6 +5,6 @@ class songsWorker
   def perform(songs_file)
     CSV.foreach(songs_file, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
-    en
+    end
   end
 end
